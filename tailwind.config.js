e4da95571f56796022ee73218a716431a0003356
NeilChenCN <neil.chen.nj@gmail.com>
2021-06-15 15:56:44 +0800

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,5 @@
 module.exports = {
-  purge: [
-    './src/components/**/*.tsx',
-
-    // This is a convention for files that only include Styled Components.
-    // If you don't use this convention feel free to skip it.
-    './src/components/**/*.styled.tsx',
-  ],
+  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
